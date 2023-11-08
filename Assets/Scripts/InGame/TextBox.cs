2518c7215909c49c5f6bbbd85da2f4ba1e1d84b5
KikyoNanakusa <nanakusa.vrc@gmail.com>
2023-11-08 17:32:36 +0900

    Add facial expresssion

--- Assets/Scripts/InGame/TextBox.cs
+++ Assets/Scripts/InGame/TextBox.cs
@@ -4,14 +4,19 @@ using System.Collections.Generic;
 using Cysharp.Threading.Tasks;
 using TMPro;
 using UnityEngine;
-using UnityEngine.PlayerLoop;
+using UnityEngine.UI;
 
 public class TextBox : MonoBehaviour
 {
+    public GameObject renderChan;
+    [SerializeField]
+    public Sprite[] renderChanSprites;
+    
     private bool _isClicked;
     private bool _isClickable = false;
     private IEnumerator<string> _script;
     private TMP_Text _thisText;
+    private Image _renderChanImage;
     
     public bool IsClicked
     {
@@ -27,10 +32,13 @@ public class TextBox : MonoBehaviour
     
     async void Start()
     {
+        _renderChanImage = renderChan.GetComponent<Image>();
         _thisText = this.GetComponent<TMP_Text>();
         IEnumerable<string> enumerableScript =
             (Resources.Load("Script", typeof(TextAsset)) as TextAsset).text.Split("\n");
         _script = enumerableScript.GetEnumerator();
+        _renderChanImage.sprite = renderChanSprites[0];
+        
         await UniTask.Delay(TimeSpan.FromSeconds(2));
         _isClickable = true;
         await UpdateTextOnClick();
@@ -42,8 +50,11 @@ public class TextBox : MonoBehaviour
         while (true)
         {
             await UniTask.WaitUntil(() => _isClicked, cancellationToken: token);
-            if (!_script.MoveNext()) break;
-            _thisText.text = _script.Current;
+            if (_script.MoveNext() == false) {Debug.Log("Scripts End!"); break;}
+
+            string[] script = _script.Current.Split(" ,$");
+            _thisText.text = script[0];
+            _renderChanImage.sprite = renderChanSprites[int.Parse(script[1])];
             _isClicked = false;
             Debug.Log(_script.Current);
         }
